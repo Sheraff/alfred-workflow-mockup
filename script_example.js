54565e495df9463a414be72cc4dfd71230d3e78c
Sheraff <fpellet@ensc.fr>
2014-08-12 18:14:24 +0200

    initial commit (working demo for Chrome 37.0 beta)

---
+++ script_example.js
@@ -0,0 +1,41 @@
+var script = [{
+	"alfred": {
+		"title": "coucou0",
+		"icon": "imgs/icon.png",
+		"list": [{
+			"title": "Le Coucou (1980)",
+			"sub": "Video (Movie), 800Mb, Seed: 10, Leech: 20",
+			"icon": "imgs/icon.png",
+			"selected": true
+		}, {
+			"title": "Vol au dessus d'un nid de coucou.French DVDRip [Sheraff]",
+			"icon": "imgs/icon.png"
+		}]
+	},
+	"notification": {
+		"title": "Le Coucou (1980)",
+		"sub": "Download started..."
+	},
+	"keypress": "⇥",
+	"duration": 1000
+}, {
+	"alfred": {
+		"title": "coucou0",
+		"icon": "imgs/icon.png",
+		"list": [{
+			"title": "Le Coucou (1980)",
+			"sub": "Video (Movie), 800Mb, Seed: 10, Leech: 20",
+			"icon": "imgs/icon.png",
+			"selected": true
+		}, {
+			"title": "Vol au dessus d'un nid de coucou.French DVDRip [Sheraff]",
+			"icon": "imgs/icon.png"
+		}]
+	},
+	"notification": {
+		"title": "Le Coucou (1980)",
+		"sub": "Download started..."
+	},
+	"keypress": "⇥",
+	"duration": 1000
+}];
